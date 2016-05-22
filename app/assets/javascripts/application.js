174b838d23883c0a7db8fdeff9b74d3e57fb2f45
Mike Wittenauer <mikewittenauerwebdevelopment@gmail.com>
2016-05-21 20:13:45 -0400

    Add google maps javascript file to precompile javascript files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,20 +15,6 @@
 //= require turbolinks
 //= require_tree .
 
-function init_map(){
-  var myOptions = {zoom:13,center:new google.maps.LatLng(41.153836,-80.54211800000002),mapTypeId: google.maps.MapTypeId.ROADMAP};
-  map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);
-  marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(41.153836,-80.54211800000002)});
-  infowindow = new google.maps.InfoWindow({content:'<strong>Jills Mixing Bowl</strong><br>3309 Pothour Wheeler Rd. Hubbard, Ohio, 44425<br>'});
-  google.maps.event.addListener(marker, 'click', function(){infowindow.open(map,marker);});
-  infowindow.open(map,marker);
-}google.maps.event.addDomListener(window, 'load', init_map);
-
-$(document).ready(function() {
-  mobileMenuToggle();
-  galleryModel();
-});
-
 function mobileMenuToggle() {
   $('#menu-toggle').on('click', function() {
     $('#main-nav').slideToggle();
